99f4e9cadaa5f3d60c745e332713368ce6ec1655
dalsoto <daltonsoto@live.com>
2023-06-03 15:08:33 -0500

    Create object schema with Mongoose, populate MongoDB

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/models',
-    models: ['*.js', '!db.js'],
-    data: 'data', 
-    db: 'mongodb://localhost:27017/travlr'
+    modelBaseDirectory: "app_server/database/models", // model directory name
+
+    models: ["*.js", "!db.js"], // model matcher
+
+    data: "data", // data directory name
+
+    db: "mongodb://localhost:27017/travlr", // db connection url
 
 };
